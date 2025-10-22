df3e42390984412b4045af5410e9b7711fbef082
3nYVaq53 <7zjaso5@gmail.com>
2025-10-22 20:41:28 +0800

    Add files via upload
    1022 practice

---
+++ 1022.py
@@ -0,0 +1,19 @@
+#滿2000折200
+total=5000
+if total >=2000:
+        discount=(total//2000)*200
+        total=total-discount
+print("折扣後金額為",total)
+#滿4000折400
+total1=7000
+if total1 >=4000:
+        discount1=(total1//4000)*400
+        total1=total1-discount1
+print("折扣後金額為",total1)
+#滿6000折600
+total2=13000
+if total2 >=6000:
+        discount2=(total2//6000)*600
+        total2=total2-discount2
+print("折扣後金額為",total2)
+
