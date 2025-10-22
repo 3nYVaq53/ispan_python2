df63992bff8662d9125405b96da96a9a3ca2a76a
3nYVaq53 <7zjaso5@gmail.com>
2025-10-22 20:49:35 +0800

    Add files via upload
    new one

--- 1022.py
+++ 1022.py
@@ -1,19 +1,19 @@
 #滿2000折200
-total=5000
+total =  int(input("請輸入消費總金額"))
 if total >=2000:
         discount=(total//2000)*200
         total=total-discount
 print("折扣後金額為",total)
 #滿4000折400
-total1=7000
-if total1 >=4000:
-        discount1=(total1//4000)*400
-        total1=total1-discount1
-print("折扣後金額為",total1)
+
+if total >=4000:
+        discount1=(total//4000)*400
+        total=total-discount1
+print("折扣後金額為",total)
 #滿6000折600
-total2=13000
-if total2 >=6000:
-        discount2=(total2//6000)*600
-        total2=total2-discount2
-print("折扣後金額為",total2)
+
+if total >=6000:
+        discount2=(total//6000)*600
+        total=total-discount2
+print("折扣後金額為",total)
 
